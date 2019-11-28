a343a2c15cfb99b6c240375ecb52b9e11d9e7673
Carlo Enrico <carlo.enrico@uol.com.br>
2019-11-28 19:36:41 -0300

    Update

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,22 +1,14 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-<<<<<<< HEAD
   max-width: 80%;
-=======
-  max-width: 700px;
->>>>>>> 4d6c3cf83b8a1330bda84b31b718b0fbe72e75de
   background-color: none;
   border-radius: 4px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
   padding: 30px 10px;
   margin: 10px auto;
   position: relative;
-<<<<<<< HEAD
   opacity: 0.9;
-=======
-  opacity: 0.8;
->>>>>>> 4d6c3cf83b8a1330bda84b31b718b0fbe72e75de
 `
 
 export { Container }
