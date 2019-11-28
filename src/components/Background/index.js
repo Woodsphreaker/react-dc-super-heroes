dc5f01efc6616b160c1a5a9a1e3e054a05f744ff
Carlo Enrico <carlo.enrico@uol.com.br>
2019-11-28 19:39:32 -0300

    Fix: Fix some bigs

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,8 +1,4 @@
 import styled from 'styled-components'
-<<<<<<< HEAD
-=======
-
->>>>>>> 4d6c3cf83b8a1330bda84b31b718b0fbe72e75de
 import Image from '../../assets/lego.jpg'
 
 const Background = styled.div`
@@ -11,11 +7,7 @@ const Background = styled.div`
   background-position: top;
   height: 100%;
   width: 100%;
-<<<<<<< HEAD
   opacity: 0.5;
-=======
-  opacity: 0.2;
->>>>>>> 4d6c3cf83b8a1330bda84b31b718b0fbe72e75de
   position: fixed;
   top: 0;
 `
